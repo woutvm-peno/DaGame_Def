93536d725a4b42980aabc100a4b7798894a3b821
MartijnVR <martijn.vanrooy@telenet.be>
2015-11-25 15:32:16 +0100

    Register en Login afgewerkt

--- app/src/main/java/andreas/gps/Register.java
+++ app/src/main/java/andreas/gps/Register.java
@@ -1,70 +1,255 @@
 package andreas.gps;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class Register extends AppCompatActivity{
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+
+public class Register extends AppCompatActivity implements View.OnClickListener{
+
+    Integer SESSIONID = 8;
     Button register_button;
     EditText login_register_edit, password_register_edit, email_edit, password_confirm_edit;
+    Servercomm mServercomm = new Servercomm();
+    private String Username;
+    private String Password;
+    private String Email;
+    private String result = "";
+    private JSONObject DataOnServer;
+    private static final String TAG = "lmao";
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        getMessage();
 
         //login
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_low_in_rank);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        login_register_edit = (EditText)findViewById(R.id.login_register_edit);
-        password_register_edit = (EditText)findViewById(R.id.password_register_edit);
-        email_edit = (EditText)findViewById(R.id.email_edit);
-        password_confirm_edit = (EditText)findViewById(R.id.password_confirm_edit);
-        register_button = (Button)findViewById(R.id.register_button);
+        login_register_edit = (EditText) findViewById(R.id.login_register_edit);
+        password_register_edit = (EditText) findViewById(R.id.password_register_edit);
+        email_edit = (EditText) findViewById(R.id.email_edit);
+        password_confirm_edit = (EditText) findViewById(R.id.password_confirm_edit);
+        register_button = (Button) findViewById(R.id.register_button);
 
-        register_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String password_register_string = password_register_edit.getText().toString();
-                String password_confirm_string = password_confirm_edit.getText().toString();
-                String login_edit_string = login_register_edit.getText().toString();
-                String email_edit_string = email_edit.getText().toString();
 
-                if (password_confirm_string.equals("") || password_register_string.equals("") || login_edit_string.equals("")|| email_edit_string.equals("")){
-                    Toast.makeText(getApplicationContext(), "Please fill in all fields.", Toast.LENGTH_SHORT).show();
+        register_button.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        String password_register_string = password_register_edit.getText().toString();
+        String password_confirm_string = password_confirm_edit.getText().toString();
+        String login_edit_string = login_register_edit.getText().toString();
+        String email_edit_string = email_edit.getText().toString();
+
+
+        if (password_confirm_string.equals("") || password_register_string.equals("") || login_edit_string.equals("") || email_edit_string.equals("")) {
+            Toast.makeText(getApplicationContext(), "Please fill in all fields.", Toast.LENGTH_SHORT).show();
+
+        } else if (!password_register_string.equals(password_confirm_string)) {
+            Toast.makeText(getApplicationContext(), "Passwords do not match!", Toast.LENGTH_SHORT).show();
+
+        } else if (email_edit_string.indexOf("@") <= -1) {
+            Toast.makeText(getApplicationContext(), "Please fill in a valid email adress.", Toast.LENGTH_SHORT).show();
+
+        } else if (containsUsername(login_edit_string)){
+            Toast.makeText(getApplicationContext(), "Username already exists.", Toast.LENGTH_SHORT).show();
+
+        } else if (containsEmail(email_edit_string)) {
+            Toast.makeText(getApplicationContext(), "This email address is already in use.", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Username = login_register_edit.getText().toString();
+            Password = password_register_edit.getText().toString();
+            Email = email_edit.getText().toString();
+            putMessage();
+            Toast.makeText(getApplicationContext(), "Registered succesfully!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public boolean containsUsername(String input){
+        boolean contains = false;
+        Integer n = 0;
+        try{
+            JSONArray Obj = DataOnServer.getJSONArray("data");
+            while (n <= Obj.length()){
+
+                JSONObject data = (JSONObject) Obj.get(n);
+                contains = data.getJSONObject("data").getString("Username").equals(input);
+
+                if (contains){
+                    break;
                 }
-
-                else if (!password_register_string.equals(password_confirm_string)){
-                    Toast.makeText(getApplicationContext(),"Passwords do not match!",Toast.LENGTH_SHORT).show();
-
-                }
-
-                else if (email_edit_string.indexOf("@") <= -1){
-                    Toast.makeText(getApplicationContext(),"Please fill in a valid email adress.",Toast.LENGTH_SHORT).show();
-
-                }
-
-                else{
-                    create_account(login_edit_string,password_register_string,email_edit_string);
-                }
-
+                n+=1;
             }
-        });
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return contains;
     }
 
-    public void create_account(String username, String password, String email){
+    public boolean containsEmail(String input){
+        boolean contains = false;
+        Integer n = 0;
+        try{
+            JSONArray Obj = DataOnServer.getJSONArray("data");
+            while (n <= Obj.length()){
 
+                JSONObject data = (JSONObject) Obj.get(n);
+                contains = data.getJSONObject("data").getString("Email").equals(input);
+
+                if (contains){
+                    break;
+                }
+                n+=1;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return contains;
     }
+
+    public void putMessage() {
+        new PutAsyncTask().execute("http://daddi.cs.kuleuven.be/peno3/data/B3_test/" + SESSIONID);
+    }
+
+    public void getMessage(){
+        //set your own group number and session ID http://daddi.cs.kuleuven.be/peno3/data/{group number}/{session ID}
+        new GetAsyncTask().execute("http://daddi.cs.kuleuven.be/peno3/data/B3_test/" + SESSIONID);
+    }
+
+    public void backToLogin(){
+        Intent intent = new Intent(this, login.class);
+        startActivity(intent);
+    }
+
+    public String createAccount(String Username, String Password, String Email, String URL) {
+
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("Username", Username);
+            obj.put("Password", Password);
+            obj.put("Email", Email);
+        } catch (JSONException e) {
+            e.printStackTrace();
+
+        }
+
+        try {
+            URL url = new URL(URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("PUT");
+            //conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            OutputStream os = conn.getOutputStream();
+
+            os.write(obj.toString().getBytes());
+            os.flush();
+
+            //Read the acknowledgement message after putting data to server
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+        String status = "Put Data to server succesfully";
+        return status;
+    }
+
+    public String getDataFromServer(String URL){
+
+        try {
+            URL url = new URL(URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            while ((output = br.readLine()) != null) {
+                result += output;
+            }
+            conn.disconnect();
+
+        } catch (Exception e) {
+        }
+        return result;
+    }
+
+    class PutAsyncTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            return createAccount(Username, Password, Email, urls[0]);}
+
+        protected void onPostExecute(String result) {
+            backToLogin();
+        }
+    }
+
+    class GetAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            return getDataFromServer(urls[0]);
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            try{
+                DataOnServer = new JSONObject(result);
+            }
+            catch (Exception e){}
+        }
+    }
+
+
 }
+
--- app/src/main/java/andreas/gps/login.java
+++ app/src/main/java/andreas/gps/login.java
@@ -1,6 +1,7 @@
 package andreas.gps;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,12 +9,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class login extends AppCompatActivity implements View.OnClickListener{
 
+    Integer SESSIONID = 8;
     Button login_button;
     EditText login_edit, password_edit;
     TextView register_text;
+    private String result = "";
+    private JSONObject DataOnServer;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +47,8 @@ public class login extends AppCompatActivity implements View.OnClickListener{
         login_button.setOnClickListener(this);
         register_text.setOnClickListener(this);
 
+        getMessage();
+
     }
 
 
@@ -40,6 +56,11 @@ public class login extends AppCompatActivity implements View.OnClickListener{
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.login_button:
+                String login_edit_string = login_edit.getText().toString();
+                String password_edit_string = password_edit.getText().toString();
+                if (!correctLogin(login_edit_string,password_edit_string)){
+                    Toast.makeText(getApplicationContext(), "Username or password is incorrect.", Toast.LENGTH_SHORT).show();
+                }
 
                 break;
 
@@ -51,4 +72,78 @@ public class login extends AppCompatActivity implements View.OnClickListener{
                 break;
         }
     }
+
+    public boolean correctLogin(String username, String password){
+        boolean correct = false;
+        boolean user;
+        boolean pass;
+        Integer n = 0;
+        try{
+            JSONArray Obj = DataOnServer.getJSONArray("data");
+            while (n <= Obj.length()){
+
+                JSONObject data = (JSONObject) Obj.get(n);
+                user = data.getJSONObject("data").getString("Username").equals(username);
+                pass = data.getJSONObject("data").getString("Password").equals(password);
+
+                if (user && pass){
+                    correct = true;
+                    break;
+                }
+                n+=1;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return correct;
+    }
+
+    public void getMessage(){
+        //set your own group number and session ID http://daddi.cs.kuleuven.be/peno3/data/{group number}/{session ID}
+        new GetAsyncTask().execute("http://daddi.cs.kuleuven.be/peno3/data/B3_test/" + SESSIONID);
+    }
+
+    public String getDataFromServer(String URL){
+
+        try {
+            java.net.URL url = new URL(URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            while ((output = br.readLine()) != null) {
+                result += output;
+            }
+            conn.disconnect();
+
+        } catch (Exception e) {
+        }
+        return result;
+    }
+
+    class GetAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            return getDataFromServer(urls[0]);
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            try{
+                DataOnServer = new JSONObject(result);
+            }
+            catch (Exception e){}
+        }
+    }
+
+
 }
